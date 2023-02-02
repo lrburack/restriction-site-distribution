67a79d6dc04a1ab3c950ca79f830dc82de8d87e8
Linden Burack <linden.burack@gmail.com>
2023-02-02 18:09:34 -0500

    Backwards Searching
    This version will now identify backwards restriction sites as well. Searching the complementary strand is unnecessary due to the nature of restriction enzymes

--- main.py
+++ main.py
@@ -7,44 +7,53 @@ import matplotlib.pyplot as plt
 def find_all(a_str, sub):
     start = 0
     while True:
-        start = a_str.find(sub, start)
+        start = seq.find(sub, start)
         if start == -1: return
         yield start
         start += len(sub)
 
+
+def site_instances(seq, site_seq):
+    # Finds forwards and backwards
+    instances = list(find_sub(record.seq, restriction_seq)) + list(find_sub(record.seq, restriction_seq[::-1]))
+    instances.sort()
+    return instances
+
 # Read the record
 record = SeqIO.read("Sequences/Long.gb", "genbank")
+
 print("Sequence " + record.name + " of length " + str(len(record.seq)))
 # Change this depending on the restriction enzyme being used
 restriction_seq = "GATC"
 
 # Count instances
-instances = list(find_all(record.seq, restriction_seq))
+instances = site_instances(record.seq, restriction_seq)
 
 # Create empty distances array
-distances = [None] * (len(instances) - 1)
-# Find distances
-for i in range(1, len(instances)):
-    distances[i-1] = instances[i] - instances[i-1]
+distances = numpy.diff(instances)
 
-print("Instances: " + str(len(instances)))
+print("\nInstances: " + str(len(instances)))
 if len(instances) != 0:
     # Actual
-    # print("Distances: " + str(distances))
     print("Mean Distance: " + str(numpy.mean(distances)))
-
     # Expected Values
     expected_instances = len(record.seq) / len(restriction_seq) * 1/(4 ** len(restriction_seq))
     expected_distance = len(record.seq) / expected_instances
     print("Expected Instances: " + str(expected_instances))
     print("Expected Mean Distance: " + str(expected_distance))
 
+# Forward only log linear   Forward only log log
+# Both log linear           Both log log
+fig, (b_loglin, b_loglog) = plt.subplots(2)
 counts, bins = np.histogram(distances, bins=100)
-plt.stairs(counts, bins)
-print("Bins: " + str(len(bins)))
-print("Counts: " + str(len(counts)))
-plt.yscale("log")
-plt.ylabel("Frequency")
-plt.xlabel("Distances")
+b_loglin.stairs(counts, bins)
+b_loglog.stairs(counts, bins)
+b_loglin.set_yscale("log")
+b_loglog.set_yscale("log")
+b_loglog.set_xscale("log")
+b_loglin.set_ylabel("Frequency")
+b_loglog.set_ylabel("Frequency")
+b_loglin.set_xlabel("Distances")
+b_loglog.set_xlabel("Distances")
 # plt.hist(bins[:-1], bins, weights=counts)
 plt.show()
